3aea4d906c72767d2d802b15f57db312b389ed8c
Wilfredo <wilfredopintomata@gmail.com>
2025-04-07 22:51:34 +0200

    Form validation was added to all fields

---
+++ index..js
@@ -0,0 +1,20 @@
+(() => {
+    'use strict'
+    console.log("La función se está ejecutando automáticamente al cargar.");
+
+    const forms = document.querySelectorAll('.needs-validation');
+    const alertBox = document.getElementById('form-alert');
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+                alertBox.classList.remove('d-none');
+            } else {
+                alertBox.classList.add('d-none');
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})()
